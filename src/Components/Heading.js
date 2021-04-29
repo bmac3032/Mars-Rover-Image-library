9530f42c456a3af3dedaf73f4ffe30fd5bfeb435
Brian McPherson <heat3032@gmail.com>
2021-04-29 14:21:53 -0400

    Added styling from Bootstrap

--- src/Components/Heading.js
+++ src/Components/Heading.js
@@ -3,9 +3,9 @@ import React from "react";
 const Heading = () => {
   return (
     <header>
-      <h1>Mars Rover Photos</h1>
-      <p>These photos are taken from the surface of mars</p>
-      <p>All photos are courtesy of NASA</p>
+      <h1 className="display-4">Mars Rover Photos</h1>
+      <p className="lead">These photos are taken from the surface of mars</p>
+      <p className="lead">All photos are courtesy of NASA</p>
     </header>
   );
 };
--- src/Components/PhotoGrid.js
+++ src/Components/PhotoGrid.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { SRLWrapper } from "simple-react-lightbox";
 import data from "../data/photos";
 
 const PhotoGrid = () => {
@@ -8,11 +9,32 @@ const PhotoGrid = () => {
   }, []);
   console.log(photos);
   return (
-    <div>
-      {photos.map((p) => {
-        return <img src={p.img_src}></img>;
-      })}
-    </div>
+    <SRLWrapper>
+      <div className="container">
+        <div className="row">
+          {photos.map((p) => {
+            return (
+              <div className="col-sm">
+                <div className="card mt-2 p-2">
+                  <img
+                    className="card-img-top"
+                    src={p.img_src}
+                    alt={`Image id: ${p.id}`}
+                  ></img>
+                  <div className="card-body">
+                    <div className="card-text">
+                      <p>ID: {p.id}</p>
+                      <p>Rover: {p.rover.name}</p>
+                      <p>Earth Date Taken: {p.earth_date}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </SRLWrapper>
   );
 };
 
